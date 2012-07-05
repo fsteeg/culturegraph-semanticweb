d6a551e7eec6b99e89dcdec1966608c14ab0462c
boehmec <boehmec@f2a00648-b9af-4511-a9bd-0c59966c402f>
2012-07-05 08:10:12 +0000

    * added example to show how to read rdf data
    git-svn-id: https://svn.code.sf.net/p/culturegraph/code/semanticweb/trunk@1341 f2a00648-b9af-4511-a9bd-0c59966c402f

---
+++ src/main/java/org/culturegraph/semanticweb/example/RdfReader.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.culturegraph.semanticweb.example;
+
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.culturegraph.metastream.sink.StreamWriter;
+import org.culturegraph.metastream.source.HTTPGetter;
+import org.culturegraph.semanticweb.pipe.JenaModel;
+import org.culturegraph.semanticweb.pipe.JenaModelToStream;
+
+/**
+ * @author Christoph Böhme
+ *
+ */
+public final class RdfReader {
+
+	private static final String EXAMPLE_URL = "http://d-nb.info/gnd/121649091/about/rdf";
+	
+	private RdfReader() {
+		// Nothing to do
+	}
+	
+	public static void main(final String[] args) throws URISyntaxException  {
+		String url = EXAMPLE_URL;
+		if (args.length >= 1) {
+			url = args[0];
+		}
+		
+		final HTTPGetter httpGetter = new HTTPGetter();
+		final JenaModel jenaModel = new JenaModel();
+		final JenaModelToStream modelToStream = new JenaModelToStream();
+		final StreamWriter streamWriter = new StreamWriter(new OutputStreamWriter(System.out));
+		
+		httpGetter
+				.setReceiver(jenaModel)
+				.setReceiver(modelToStream)
+				.setReceiver(streamWriter);
+		
+		httpGetter.process(new URI(url));
+		httpGetter.closeResources();
+	}
+	
+}
